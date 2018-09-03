e0d73610466bfab6e282d275d05e9bdcdf1370e3
Bluey95 <susanwere15@gmail.com>
2018-09-03 23:18:22 +0300

    first stage

--- app/src/main/java/com/example/susan/popularmovies/Utilities/NetworkUtils.java
+++ app/src/main/java/com/example/susan/popularmovies/Utilities/NetworkUtils.java
@@ -1,65 +1,95 @@
 package com.example.susan.popularmovies.Utilities;
 
-import android.nfc.Tag;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 import com.example.susan.popularmovies.Movie;
+import com.example.susan.popularmovies.databaseSQLITE.MovieContract;
+import com.example.susan.popularmovies.databaseSQLITE.MovieDbHelper;
 
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static android.content.ContentValues.TAG;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class NetworkUtils {
 
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+    private static MovieDbHelper mOpenHelper;
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
     public static ArrayList<Movie> fetchData(String url) throws IOException {
         ArrayList<Movie> movies = new ArrayList<Movie>();
         try {
-            URL new_url = new URL(url); //create a url from a string
-            HttpURLConnection connection = (HttpURLConnection) new_url.openConnection(); //open http connection to object
+
+            URL new_url = new URL(url); //create a url from a String
+            HttpURLConnection connection = (HttpURLConnection) new_url.openConnection(); //Opening a http connection  to the remote object
             connection.connect();
-            InputStream inputStream = connection.getInputStream();
-            //read from the object
-            String results = IOUtils.toString(inputStream);//convert input stream objects to strings
-            parseJson(results, movies);
+
+            InputStream inputStream = connection.getInputStream(); //reading from the object
+            String results = IOUtils.toString(inputStream);  //IOUtils to convert inputstream objects into Strings type
+            parseJson(results,movies);
             inputStream.close();
-        } catch (IOException e){
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return movies;
     }
 
-    private static void parseJson(String data, ArrayList<Movie> list) {
-        try{
+    public static void parseJson(String data, ArrayList<Movie> list){
+
+
+        try {
             JSONObject mainObject = new JSONObject(data);
-            JSONArray resArray = mainObject.getJSONArray("results");
-            //Getting the results object
-            for (int i = 0; i < resArray.length(); i++){
+            Log.v(TAG,mainObject.toString());
+            JSONArray resArray = mainObject.getJSONArray("results"); //Getting the results object
+            for (int i = 0; i < resArray.length(); i++) {
                 JSONObject jsonObject = resArray.getJSONObject(i);
-                Movie movie = new Movie(); //new movie object
-                movie.setId(jsonObject.getInt("id"));
-                movie.setBackdropPath(jsonObject.getString("backdrop_path"));
+                Movie movie = new Movie(); //New Movie object
+                movie.setId(jsonObject.getLong("id"));
+                movie.setVoteAverage(jsonObject.getString("vote_average"));
                 movie.setOriginalTitle(jsonObject.getString("original_title"));
+                movie.setBackdropPath(jsonObject.getString("backdrop_path"));
                 movie.setOverview(jsonObject.getString("overview"));
-                movie.setPopularity(jsonObject.getDouble("popularity"));
-                movie.setPosterPath(jsonObject.getString("poster_path"));
                 movie.setReleaseDate(jsonObject.getString("release_date"));
-                movie.setVoteAverage(jsonObject.getInt("vote_average"));
-                movie.setVoteCount(jsonObject.getInt("vote_count"));
-                movie.setTitle(jsonObject.getString("title"));
+                movie.setPosterPath(jsonObject.getString("poster_path"));
+                //Adding a new movie object into ArrayList
+                list.add(movie);
             }
         } catch (JSONException e) {
             e.printStackTrace();
-            Log.e(TAG, "Error occured durin json parsing", e);
+            Log.e(TAG, "Error occurred during JSON Parsing");
         }
+
     }
+
+
+    public static Boolean networkStatus(Context context){
+        ConnectivityManager manager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()){
+            return true;
+        }
+        return false;
+    }
+
 }
